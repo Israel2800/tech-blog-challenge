84f3566df3677035da7dbe09abb4cac251a1437b
Israel Aguilar <dady281100@gmail.com>
2023-07-22 22:35:29 -0600

    Adding comment to specify the routes in dashboard-routes.js file

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,7 +7,7 @@ const {
 } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+// Dasher routes
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
             where: {
